bb7394c8afbdd9b61a61818ec191c59c78bcbf05
Keerthi <161058505+keerthi1722@users.noreply.github.com>
2025-10-20 10:31:29 +0530

    Update UserDashboard.jsx

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import API from '../lib/apiBase'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
